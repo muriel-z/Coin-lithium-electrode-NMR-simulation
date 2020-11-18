7f180493c26393085cfe87af922e98eec27030f1
muriel-z <murielazmp@gmail.com>
2020-11-17 22:53:16 -0300

    Agregué el registro de los máximos y mínimos para el informe. Emprolijé otras cositas

--- simulación_bulk.py
+++ simulación_bulk.py
@@ -13,7 +13,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import calculateFieldShift as cFS 
 from SimulationVolume import *
 
-#Voy a construir una celda cilindrica, con las dimensiones h=0.71mm
+#Voy a construir un electrodo cilindrica, con las dimensiones h=0.71mm
 # y d=12mm. Además quiero FOVz=10*h y FOVxy=2*d
 
 
@@ -36,7 +36,7 @@ nsx = int(d/vsx)
 nsy = int(d/vsy)
 nsz = int(h/vsz)
 
-# Defino los indices que conforman la celda 
+# Defino los indices que conforman el electrodo 
 
 indices = []
 
@@ -137,7 +137,7 @@ plt.ylabel('Delta [ppm]')
 plt.title('Delta(z)')
 ax.legend()
 
-#Veo unicamente la variación dentro de la celda para d_in
+#Veo unicamente la variación dentro del electrodo para Delta_in
 plt.figure(31)
 x = np.arange(3.45,4.26,0.03)
 z = np.arange(115,142,1)
@@ -153,10 +153,10 @@ ax.plot(x,i,'-',linewidth=2, label=' R = 6 mm')
 ax.set_ylim(-12, 7)
 plt.xlabel('z [mm]')
 plt.ylabel('Delta [ppm]')
-plt.title('Delta(z) dentro de la celda')
+plt.title('Delta(z) dentro del electrodo')
 ax.legend()
 
-#Veo unicamente la variación fuera de la celda para d_out
+#Veo unicamente la variación fuera del electrodo para Delta_out
 plt.figure(32)
 x = np.arange(4.17,7.68,0.03)
 z = np.arange(139,256,1)
@@ -172,11 +172,145 @@ ax.plot(x,i,'-',linewidth=2, label=' R = 6 mm')
 ax.set_ylim(-12, 7)
 plt.xlabel('z [mm]')
 plt.ylabel('Delta [ppm]')
-plt.title('Delta(z) fuera de la celda')
+plt.title('Delta(z) fuera del electrodo')
 ax.legend()
+
+#Gráficos para figuras esquemáticas
+#Delta_in
+
+plt.figure(33)
+x = np.arange(3.45,4.26,0.03)
+z = np.arange(115,142,1)
+f = delta[z,int(N[1]/2),int(N[2]/2)]
+ax = plt.subplot(111)
+ax.plot(x,f,'-',linewidth=2, label=' R = 0 mm')
+ax.set_ylim(-12, 7)
+plt.xlabel('z [mm]')
+plt.ylabel('Delta [ppm]')
+plt.title('Delta(z) dentro del electrodo')
+ax.legend()
+
+#Delta_out
+plt.figure(34)
+x = np.arange(4.17,7.68,0.03)
+z = np.arange(139,256,1)
+f = delta[z,int(N[1]/2),int(N[2]/2)]
+ax = plt.subplot(111)
+ax.plot(x,f,'-',linewidth=2, label=' R = 0 mm')
+ax.set_ylim(-12, 7)
+plt.xlabel('z [mm]')
+plt.ylabel('Delta [ppm]')
+plt.title('Delta(z) fuera del electrodo')
+ax.legend()
+
+#Delta_centro
+
+plt.figure(35)
+x = np.arange(0,7.68,0.03)
+f = delta[:,int(N[1]/2),int(N[2]/2)]
+h = delta[:,int(N[1]/2),int(N[2]*173/256)]
+ax = plt.subplot(111)
+ax.plot(x,f,'-',linewidth=2, label=' R = 0 mm')
+ax.plot(x,h,'-',linewidth=2, label=' R = 4.5 mm')
+ax.set_ylim(-12, 7)
+plt.xlabel('z [mm]')
+plt.ylabel('Delta [ppm]')
+plt.title('Delta(z)')
+ax.legend()
+
+#%%
+#En esta sección determinamos los valores exactos para calcular Delta_in /
+#Delta_out y Delta_centro
+#NOTACIÓN: para clarificar los máximos y mínimos, iran con el subindice
+# de su función seguido de los números 1 correspondiente a Delta_in,
+# 2 a Delta_out y Delta_centro se calcula con estos valores
+
+#Delta_in
+#min
+z = np.arange(115,142,1)
+f = delta[z,int(N[1]/2),int(N[2]/2)]
+g = delta[z,int(N[1]/2),int(N[2]*79/128)]
+h = delta[z,int(N[1]/2),int(N[2]*173/256)]
+i = delta[z,int(N[1]/2),int(N[2]*47/64)]
+
+minf1 = np.min(f)
+print(minf1,['minf1'])
+ming1 = np.min(g)
+print(ming1,['ming1'])
+minh1 = np.min(h)
+print(minh1,['minh1'])
+mini1 = np.min(i)
+print(mini1,['mini1'])
+#max
+z = np.arange(120,136,1)
+f = delta[z,int(N[1]/2),int(N[2]/2)]
+g = delta[z,int(N[1]/2),int(N[2]*79/128)]
+h = delta[z,int(N[1]/2),int(N[2]*173/256)]
+i = delta[z,int(N[1]/2),int(N[2]*47/64)]
+
+maxf1 = np.max(f)
+print(maxf1,['maxf1'])
+maxg1 = np.max(g)
+print(maxg1,['maxg1'])
+maxh1 = np.max(h)
+print(maxh1,['maxh1'])
+maxi1 = np.max(i)
+print(maxi1,['maxi1'])
+
+#Diferencias
+Delta_in_f = maxf1-minf1 
+print(Delta_in_f,['Delta_in_f'])
+Delta_in_g = maxg1-ming1
+print(Delta_in_g,['Delta_in_g'])
+Delta_in_h = maxh1-minh1
+print(Delta_in_h,['Delta_in_h'])
+Delta_in_i = maxi1-mini1
+print(Delta_in_i,['Delta_in_i'])
+
+#Delta_out
+#min
+z = np.arange(142,256,1)
+f = delta[z,int(N[1]/2),int(N[2]/2)]
+g = delta[z,int(N[1]/2),int(N[2]*79/128)]
+h = delta[z,int(N[1]/2),int(N[2]*173/256)]
+i = delta[z,int(N[1]/2),int(N[2]*47/64)]
+
+minf2 = np.min(f)
+print(minf2,['minf2'])
+ming2 = np.min(g)
+print(ming2,['ming2'])
+minh2 = np.min(h)
+print(minh2,['minh2'])
+mini2 = np.min(i)
+print(mini2,['mini2'])
+#max
+z = np.arange(139,256,1)
+f = delta[z,int(N[1]/2),int(N[2]/2)]
+g = delta[z,int(N[1]/2),int(N[2]*79/128)]
+h = delta[z,int(N[1]/2),int(N[2]*173/256)]
+i = delta[z,int(N[1]/2),int(N[2]*47/64)]
+
+maxf2 = np.max(f)
+print(maxf2,['maxf2'])
+maxg2 = np.max(g)
+print(maxg2,['maxg2'])
+maxh2 = np.max(h)
+print(maxh2,['maxh2'])
+maxi2 = np.max(i)
+print(maxi2,['maxi2'])
+
+#Diferencias
+Delta_out_f = maxf2-minf2 
+print(Delta_out_f,['Delta_out_f'])
+Delta_out_g = maxg2-ming2
+print(Delta_out_g,['Delta_out_g'])
+Delta_out_h = maxh2-minh2
+print(Delta_out_h,['Delta_out_h'])
+Delta_out_i = maxi2-mini2
+print(Delta_out_i,['Delta_out_i'])
 
 
 
 #%%
-  # al final del codigo, para que se vean los gradicos hay que poner
+ 
 plt.show()
